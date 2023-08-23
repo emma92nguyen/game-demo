4572b5bde4a4f36cc90bd435e839df715e9d70bd
emma92nguyen <emma92nguyen@gmail.com>
2023-08-23 23:21:49 +0200

    init backend

---
+++ rock-paper-scissors-game/src/test/java/demo/rockpaperscissorsgame/RockPaperScissorsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.rockpaperscissorsgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
