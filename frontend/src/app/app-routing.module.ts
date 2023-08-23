9d43c8c6b8ce40a4114e104306c5443dd3e9a472
emma92nguyen <emma92nguyen@gmail.com>
2023-08-23 23:24:01 +0200

    add routes

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {RockPaperScissorsGameComponent} from "./components/rock-paper-scissors-game/rock-paper-scissors-game.component";
+import {PlayerRankingComponent} from "./components/player-ranking/player-ranking.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: 'game', pathMatch: 'full'},
+  { path: 'game', component: RockPaperScissorsGameComponent },
+  { path: 'ranking', component: PlayerRankingComponent },
+  { path: '**', component: RockPaperScissorsGameComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
